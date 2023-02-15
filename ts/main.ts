93c78574298bc1a26bcf1e9f60b7f684bee5a169
Juan OssaO <juan.ossao@perficient.com>
2023-02-15 15:07:03 -0500

    Obtiene los codigos numericos ingresando las barras

--- ts/main.ts
+++ ts/main.ts
@@ -3,9 +3,19 @@ console.log("Test TS")
 const BASE = 10
 
 function getIdSum(numbers:string) {
-    for (const stringNumber of numbers) {
-
+    let id=0;
+    let b = 0;
+    let c = 0;
+    for (let i = 0; i <= numbers.length-2; i+=2) {
+        if(i+1<numbers.length){
+            b= Math.pow(BASE,parseInt(numbers[i+1]))
+        }
+        if(i+2<numbers.length){
+            c= parseInt(numbers[i+2])
+        }
+        id += b * c;
     }
+    return id + parseInt(numbers[0])
 }
 const processBarCode = (barCode:string) => {
     let elementCounter=1;
@@ -18,9 +28,6 @@ const processBarCode = (barCode:string) => {
             elementCounter++;
         }
         else{
-            if(barCode[i]==" "){
-                elementCounter=Math.pow(BASE,elementCounter)
-            }
             numbersString+=elementCounter
             elementCounter=1
         }
@@ -28,12 +35,11 @@ const processBarCode = (barCode:string) => {
     if(barCode.slice(-1)== " "){
         numbersString+=0
     }
-    return numbersString
+    return getIdSum(numbersString)
 }
 function getBarIdsArray(codeBarList:string[]=[]) {
     for (const barCode of codeBarList) {
         console.log(processBarCode(barCode)) 
     }
 }
-//console.log("Hiii")
 getBarIdsArray(codeBarList)
